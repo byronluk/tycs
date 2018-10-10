e76288f0aa640c12cc8747d9474c0bb353fb2c12
byronluk <byronluk@gmail.com>
2018-10-09 21:16:03 -0700

    1.03

--- programming/sicp/chapter-1/1.03.scm
+++ programming/sicp/chapter-1/1.03.scm
@@ -1,2 +1,19 @@
 #lang scheme
 
+; Define a procedure that takes three numbers as arguments
+; and returns the sum of the squares of the two larger numbers.
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+ (+ (square x) (square y)))
+
+(define (sum-of-squares-largest x y z)
+ (cond ((and (<= x y) (<= x z)) (sum-of-squares y z))
+       ((and (<= y x) (<= y z)) (sum-of-squares x z))
+       (else (sum-of-squares x y))))
+
+(sum-of-squares-largest 1 2 3) ; 13
+(sum-of-squares-largest 2 2 4) ; 20
+(sum-of-squares-largest 1 6 1) ; 37
+(sum-of-squares-largest 2 2 2) ; 8

79a9d295c8b4c775afd5b3a1ad5fcf7f915598d5
byronluk <byronluk@gmail.com>
2018-10-09 21:21:24 -0700

    sicp 1.04

---
+++ programming/sicp/chapter-1/1.04.scm
@@ -0,0 +1,10 @@
+#lang scheme
+
+; Observe that our model of evaluation allows for combinations whose operators are compound expressions.
+; Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+; if b is greater than 0, use the addition operator
+; if b is less than 0, use the minus operator

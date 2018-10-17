e06f1a58869309299e8ec54da14336954724b66c
byronluk <byronluk@gmail.com>
2018-10-17 15:06:10 -0700

    sicp 1.08

---
+++ programming/sicp/chapter-1/1.08.scm
@@ -0,0 +1,31 @@
+#lang scheme
+
+(define (good-enough? guess x)
+ (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+ (define new-guess (improve guess x))
+ (if (new-good-enough? guess new-guess)
+  guess
+  (sqrt-iter new-guess x)))
+
+(define (improve guess x)
+ (/ (+ (/ x (square guess))
+     (* 2 guess))
+  3))
+
+(define (average x y)
+ (/ (+ x y) 2))
+
+(define (new-good-enough? guess new-guess)
+ (< (/ (abs (- guess new-guess)) guess) 0.001))
+
+(define (square x) (* x x))
+
+(define (sqrt x)
+ (sqrt-iter 1.0 x))
+
+(sqrt 64) ; 4
+(sqrt 27) ; 3
+(sqrt 8) ; 2
+(sqrt 0.001) ; 0.1

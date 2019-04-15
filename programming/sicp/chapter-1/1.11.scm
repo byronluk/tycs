4df2ac340457f118506abced204264e92df2a42f
byronluk <byronluk@gmail.com>
2019-04-14 20:25:05 -0700

    1.11, 1.12

---
+++ programming/sicp/chapter-1/1.11.scm
@@ -0,0 +1,24 @@
+#lang scheme
+
+; A function f is defined by the rule that f(n)=n if n<3 and
+; f(n)=f(n−1)+2f(n−2)+3f(n−3) if n≥3.
+; Write a procedure that computes f by means of a recursive process.
+; Write a procedure that computes f by means of an iterative process.
+
+; recursive process
+(define (f n)
+ (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+
+; iterative process
+(define (f n)
+ (f-iter 0 1 2 n))
+
+(define (f-iter a b c count)
+ (define next (+ c (* 2 b) (* 3 a)))
+ (if (= count 0)
+      a
+      (f-iter b c next (- count 1))))

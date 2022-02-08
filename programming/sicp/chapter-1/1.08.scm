3b20ded6d8fbe37761dc0139fe4d82f3a833355e
byronluk <byronluk@gmail.com>
2022-02-07 22:32:02 -0800

    clean up exercise 1.08.scm

--- programming/sicp/chapter-1/1.08.scm
+++ programming/sicp/chapter-1/1.08.scm
@@ -1,29 +1,21 @@
-(define (good-enough? guess x)
- (< (abs (- (square guess) x)) 0.001))
-
-(define (sqrt-iter guess x)
- (define new-guess (improve guess x))
- (if (new-good-enough? guess new-guess)
-  guess
-  (sqrt-iter new-guess x)))
-
 (define (improve guess x)
- (/ (+ (/ x (square guess))
-     (* 2 guess))
-  3))
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
 
-(define (average x y)
- (/ (+ x y) 2))
-
-(define (new-good-enough? guess new-guess)
+(define (good-enough? guess new-guess)
  (< (/ (abs (- guess new-guess)) guess) 0.001))
 
-(define (square x) (* x x))
+(define (cube-iter guess x)
+ (define new-guess (improve guess x))
+ (if (good-enough? guess new-guess)
+  guess
+  (cube-iter new-guess x)))
 
-(define (sqrt x)
- (sqrt-iter 1.0 x))
+(define (cube-root x)
+ (cube-iter 1.0 x))
 
-(sqrt 64) ; 4
-(sqrt 27) ; 3
-(sqrt 8) ; 2
-(sqrt 0.001) ; 0.1
+(cube-root 8)
+
+(cube-root 64) ; 4
+(cube-root 27) ; 3
+(cube-root 8) ; 2
+(cube-root 0.001) ; 0.1
